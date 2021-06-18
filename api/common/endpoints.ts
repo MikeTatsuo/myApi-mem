6bdc4be60dd84fc780fd9e1320cd11c48f18326f
MikeTatsuo <mike_tatsuo@hotmail.com>
2021-06-18 02:20:05 -0300

    refactor: creating endpoints for single items and array of items

--- api/common/endpoints.ts
+++ api/common/endpoints.ts
@@ -1,8 +1,11 @@
 export const Endpoints = {
 	auth: '/auth',
 	authRefresh: '/refresh-token',
+	histories: '/histories',
 	history: '/history',
 	root: '/',
 	task: '/task',
+	tasks: '/tasks',
 	users: '/users',
+	user: '/user',
 };
