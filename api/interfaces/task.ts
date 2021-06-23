43541df5804f9e5e0ff16e8bbb33d29c4707ac77
MikeTatsuo <mike_tatsuo@hotmail.com>
2021-06-23 02:03:02 -0300

    feat: added property historyId to task interface

--- api/interfaces/task.ts
+++ api/interfaces/task.ts
@@ -4,6 +4,7 @@ import { TaskTime } from './task.time';
 export interface Task extends BaseObject {
 	finished: boolean;
 	name: string;
+	historyId?: number;
 	observation?: string;
 	timeTable?: TaskTime[];
 }
