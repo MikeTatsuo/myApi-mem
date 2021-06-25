85b621629a79a68d52714d4d5e7ad1b41b8ddb3f
MikeTatsuo <mike_tatsuo@hotmail.com>
2021-06-25 02:35:40 -0300

    test: refactoring user.delete.test

--- test/user/user.delete.test.ts
+++ test/user/user.delete.test.ts
@@ -6,87 +6,58 @@ import { Endpoints, ErrorMsgs, HttpCodes } from '../../api/common';
 import { UserMock } from '../../mock';
 
 let firstUserId = 0;
-const invalidUserId = 0;
 
 const header = { Authorization: 'Bearer ' };
 const { firstUser, secondUser } = UserMock;
-const { username, email, password } = firstUser;
-const { ARRAY, NUMBER, OBJECT, STRING } = EnumOfTypes;
-const { auth, user, users } = Endpoints;
-const { BAD_REQUEST, CREATED, FORBIDDEN, NOT_FOUND, OK, UNAUTHORIZED } = HttpCodes;
+const { email, password } = firstUser;
+const { NUMBER, OBJECT, STRING } = EnumOfTypes;
+const { auth, user } = Endpoints;
+const { BAD_REQUEST, FORBIDDEN, NOT_FOUND, OK, UNAUTHORIZED } = HttpCodes;
 const { authRequired, invalidSignature, missingParamId, userNotFound } = ErrorMsgs;
+const req = request(server);
 
 describe('user.delete.test', () => {
-	describe(`POST ${user}`, () => {
-		it('should return 201 - Created', (done) => {
-			request(server)
-				.post(user)
-				.send(firstUser)
-				.then(({ status, body }: Response) => {
-					const { id } = body;
-
-					expect(status).to.equal(CREATED);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(id).to.be.an(NUMBER);
-					expect(body.username).to.be.an(STRING);
-					expect(body.username).to.be.equal(username);
-					expect(body.email).to.be.an(STRING);
-					expect(body.email).to.be.equal(email);
-					expect(body).not.haveOwnProperty('password');
-
-					firstUserId = id;
-
-					done();
-				})
-				.catch(done);
-		});
-	});
-
-	describe(`POST ${auth}`, () => {
-		it('should return 201 - Created', (done) => {
-			request(server)
-				.post(auth)
-				.send({ email, password })
-				.then(({ body, status }: Response) => {
-					const { accessToken, refreshToken } = body;
-
-					expect(status).to.equal(CREATED);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(accessToken).to.be.an(STRING);
-					expect(refreshToken).to.be.an(STRING);
-
-					header.Authorization += accessToken;
-					done();
-				})
-				.catch(done);
-		});
-	});
-
 	describe(`DELETE ${user}/:userId`, () => {
 		it('should return 200 - Ok', (done) => {
-			request(server)
-				.delete(`${user}/${firstUserId}`)
-				.set(header)
-				.send()
-				.then((res: Response) => {
-					const { status, body } = res;
+			req
+				.post(user)
+				.send(firstUser)
+				.then(({ body }: Response) => {
 					const { id } = body;
+					firstUserId = id;
 
-					expect(status).to.equal(OK);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(id).to.be.an(NUMBER);
-					expect(id).to.be.equal(firstUserId);
+					req
+						.post(auth)
+						.send({ email, password })
+						.then(({ body }: Response) => {
+							const { accessToken } = body;
+							header.Authorization += accessToken;
 
-					done();
+							req
+								.delete(`${user}/${firstUserId}`)
+								.set(header)
+								.send()
+								.then((res: Response) => {
+									const { status, body } = res;
+									const { id } = body;
+
+									expect(status).to.equal(OK);
+									expect(body).not.to.be.empty;
+									expect(body).to.be.an(OBJECT);
+									expect(id).to.be.an(NUMBER);
+									expect(id).to.be.equal(firstUserId);
+
+									done();
+								})
+								.catch(done);
+						})
+						.catch(done);
 				})
 				.catch(done);
 		});
 
 		it('should return 403 - Forbidden - invalid JWT', (done) => {
-			request(server)
+			req
 				.delete(`${user}/${firstUserId}`)
 				.set({ Authorization: `${header.Authorization}123` })
 				.send(secondUser)
@@ -105,7 +76,7 @@ describe('user.delete.test', () => {
 		});
 
 		it('should return 401 - Unauthorized - no JWT set', (done) => {
-			request(server)
+			req
 				.delete(`${user}/${firstUserId}`)
 				.send(secondUser)
 				.then(({ body, status }: Response) => {
@@ -123,7 +94,7 @@ describe('user.delete.test', () => {
 		});
 
 		it('should return 400 - Bad Request - missing param id', (done) => {
-			request(server)
+			req
 				.delete(user)
 				.set(header)
 				.send()
@@ -142,8 +113,8 @@ describe('user.delete.test', () => {
 		});
 
 		it('should return 404 - Not Found - user not found', (done) => {
-			request(server)
-				.delete(`${user}/${invalidUserId}`)
+			req
+				.delete(`${user}/${firstUserId}`)
 				.set(header)
 				.send()
 				.then((res: Response) => {
@@ -155,25 +126,6 @@ describe('user.delete.test', () => {
 					expect(body).to.be.an(OBJECT);
 					expect(error).to.be.an(STRING);
 					expect(error).to.be.equal(userNotFound);
-
-					done();
-				})
-				.catch(done);
-		});
-	});
-
-	describe(`GET ${users}`, () => {
-		it('should return 200 - Ok - and body to be empty', (done) => {
-			request(server)
-				.get(users)
-				.set(header)
-				.send()
-				.then((res: Response) => {
-					const { status, body } = res;
-
-					expect(status).to.equal(OK);
-					expect(body).to.be.an(ARRAY);
-					expect(body).to.be.empty;
 
 					done();
 				})
