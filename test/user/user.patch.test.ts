384206e393251090dd75e716f417528020152f5a
MikeTatsuo <mike_tatsuo@hotmail.com>
2021-06-25 02:09:06 -0300

    test: refactoring user.patch.test

--- test/user/user.patch.test.ts
+++ test/user/user.patch.test.ts
@@ -24,106 +24,63 @@ const {
 	usernameExist,
 	userNotFound,
 } = ErrorMsgs;
+const req = request(server);
 
 describe('user.patch.test', () => {
-	describe(`POST ${user}`, () => {
-		it('should return 201 - Created', (done) => {
-			request(server)
-				.post(user)
-				.send(firstUser)
-				.then(({ status, body }: Response) => {
-					const { id } = body;
-
-					expect(status).to.equal(CREATED);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(id).to.be.an(NUMBER);
-					expect(body.username).to.be.an(STRING);
-					expect(body.username).to.be.equal(username);
-					expect(body.email).to.be.an(STRING);
-					expect(body.email).to.be.equal(email);
-					expect(body).not.haveOwnProperty('password');
-
-					firstUserId = id;
-
-					done();
-				})
-				.catch(done);
-		});
-
-		it('should return 201 - Created - 2nd user', (done) => {
-			request(server)
-				.post(user)
-				.send(secondUser)
-				.then(({ status, body }: Response) => {
-					const { id } = body;
-
-					expect(status).to.equal(CREATED);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(id).to.be.an(NUMBER);
-					expect(body.username).to.be.an(STRING);
-					expect(body.username).to.be.equal(secondUser.username);
-					expect(body.email).to.be.an(STRING);
-					expect(body.email).to.be.equal(secondUser.email);
-					expect(body).not.haveOwnProperty('password');
-
-					secondUserId = id;
-
-					done();
-				})
-				.catch(done);
-		});
-	});
-
-	describe(`POST ${auth}`, () => {
-		it('should return 201 - Created', (done) => {
-			request(server)
-				.post(auth)
-				.send({ email, password })
-				.then(({ body, status }: Response) => {
-					const { accessToken, refreshToken } = body;
-
-					expect(status).to.equal(CREATED);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(accessToken).to.be.an(STRING);
-					expect(refreshToken).to.be.an(STRING);
-
-					header.Authorization += accessToken;
-					done();
-				})
-				.catch(done);
-		});
-	});
-
 	describe(`PATCH ${user}/:userId`, () => {
 		it('should return 200 - Ok - patching username', (done) => {
-			request(server)
-				.patch(`${user}/${firstUserId}`)
-				.set(header)
-				.send({ username: thirdUser.username })
-				.then(({ body, status }: Response) => {
+			req
+				.post(user)
+				.send(firstUser)
+				.then(({ body }: Response) => {
 					const { id } = body;
+					firstUserId = id;
 
-					expect(status).to.equal(OK);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(id).to.be.an(NUMBER);
-					expect(id).to.be.equal(firstUserId);
-					expect(body.email).to.be.an(STRING);
-					expect(body.email).to.be.equal(email);
-					expect(body.username).to.be.an(STRING);
-					expect(body.username).to.be.equal(thirdUser.username);
-					expect(body).not.haveOwnProperty('password');
+					req
+						.post(user)
+						.send(secondUser)
+						.then(({ body }: Response) => {
+							const { id } = body;
+							secondUserId = id;
 
-					done();
+							req
+								.post(auth)
+								.send({ email, password })
+								.then(({ body }: Response) => {
+									const { accessToken } = body;
+									header.Authorization += accessToken;
+
+									req
+										.patch(`${user}/${firstUserId}`)
+										.set(header)
+										.send({ username: thirdUser.username })
+										.then(({ body, status }: Response) => {
+											const { id } = body;
+
+											expect(status).to.equal(OK);
+											expect(body).not.to.be.empty;
+											expect(body).to.be.an(OBJECT);
+											expect(id).to.be.an(NUMBER);
+											expect(id).to.be.equal(firstUserId);
+											expect(body.email).to.be.an(STRING);
+											expect(body.email).to.be.equal(email);
+											expect(body.username).to.be.an(STRING);
+											expect(body.username).to.be.equal(thirdUser.username);
+											expect(body).not.haveOwnProperty('password');
+
+											done();
+										})
+										.catch(done);
+								})
+								.catch(done);
+						})
+						.catch(done);
 				})
 				.catch(done);
 		});
 
 		it('should return 200 - Ok - patching email', (done) => {
-			request(server)
+			req
 				.patch(`${user}/${firstUserId}`)
 				.set(header)
 				.send({ email: thirdUser.email })
@@ -147,7 +104,7 @@ describe('user.patch.test', () => {
 		});
 
 		it('should return 200 - Ok - patching password', (done) => {
-			request(server)
+			req
 				.patch(`${user}/${firstUserId}`)
 				.set(header)
 				.send({ password })
@@ -171,7 +128,7 @@ describe('user.patch.test', () => {
 		});
 
 		it('should return 403 - Forbidden - invalid JWT', (done) => {
-			request(server)
+			req
 				.patch(`${user}/${firstUserId}`)
 				.set({ Authorization: `${header.Authorization}123` })
 				.send(secondUser)
@@ -190,7 +147,7 @@ describe('user.patch.test', () => {
 		});
 
 		it('should return 401 - Unauthorized - no JWT set', (done) => {
-			request(server)
+			req
 				.patch(`${user}/${firstUserId}`)
 				.send(secondUser)
 				.then(({ body, status }: Response) => {
@@ -208,7 +165,7 @@ describe('user.patch.test', () => {
 		});
 
 		it('should return 400 - Bad Request - empty body', (done) => {
-			request(server)
+			req
 				.patch(`${user}/${firstUserId}`)
 				.set(header)
 				.send()
@@ -227,7 +184,7 @@ describe('user.patch.test', () => {
 		});
 
 		it('should return 400 - Bad Request - username exists', (done) => {
-			request(server)
+			req
 				.patch(`${user}/${firstUserId}`)
 				.set(header)
 				.send({ username: secondUser.username })
@@ -246,7 +203,7 @@ describe('user.patch.test', () => {
 		});
 
 		it('should return 400 - Bad Request - email exists', (done) => {
-			request(server)
+			req
 				.patch(`${user}/${firstUserId}`)
 				.set(header)
 				.send({ email: secondUser.email })
@@ -265,7 +222,7 @@ describe('user.patch.test', () => {
 		});
 
 		it('should return 400 - Bad Request - invalid body request', (done) => {
-			request(server)
+			req
 				.patch(`${user}/${firstUserId}`)
 				.set(header)
 				.send({ teste: 'teste' })
@@ -284,7 +241,7 @@ describe('user.patch.test', () => {
 		});
 
 		it('should return 400 - Bad Request - missing param id', (done) => {
-			request(server)
+			req
 				.patch(user)
 				.set(header)
 				.send({ username })
@@ -303,82 +260,35 @@ describe('user.patch.test', () => {
 		});
 
 		it('should return 404 - Not Found - user not found', (done) => {
-			request(server)
-				.patch(`${user}/${invalidUserId}`)
-				.set(header)
-				.send(username)
-				.then((res: Response) => {
-					const { status, body } = res;
-					const { error } = body;
-
-					expect(status).to.equal(NOT_FOUND);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(error).to.be.an(STRING);
-					expect(error).to.be.equal(userNotFound);
-
-					done();
-				})
-				.catch(done);
-		});
-	});
-
-	describe(`DELETE ${user}/:userId`, () => {
-		it('should return 200 - Ok', (done) => {
-			request(server)
+			req
 				.delete(`${user}/${firstUserId}`)
 				.set(header)
 				.send()
-				.then((res: Response) => {
-					const { status, body } = res;
-					const { id } = body;
+				.then(() => {
+					req
+						.delete(`${user}/${secondUserId}`)
+						.set(header)
+						.send()
+						.then(() => {
+							req
+								.patch(`${user}/${firstUserId}`)
+								.set(header)
+								.send(username)
+								.then((res: Response) => {
+									const { status, body } = res;
+									const { error } = body;
 
-					expect(status).to.equal(OK);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(id).to.be.an(NUMBER);
-					expect(id).to.be.equal(firstUserId);
+									expect(status).to.equal(NOT_FOUND);
+									expect(body).not.to.be.empty;
+									expect(body).to.be.an(OBJECT);
+									expect(error).to.be.an(STRING);
+									expect(error).to.be.equal(userNotFound);
 
-					done();
-				})
-				.catch(done);
-		});
-
-		it('should return 200 - Ok - deleting second user', (done) => {
-			request(server)
-				.delete(`${user}/${secondUserId}`)
-				.set(header)
-				.send()
-				.then((res: Response) => {
-					const { status, body } = res;
-					const { id } = body;
-
-					expect(status).to.equal(OK);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(id).to.be.an(NUMBER);
-					expect(id).to.be.equal(secondUserId);
-
-					done();
-				})
-				.catch(done);
-		});
-	});
-
-	describe(`GET ${users}`, () => {
-		it('should return 200 - Ok - and body to be empty', (done) => {
-			request(server)
-				.get(users)
-				.set(header)
-				.send()
-				.then((res: Response) => {
-					const { status, body } = res;
-
-					expect(status).to.equal(OK);
-					expect(body).to.be.an(ARRAY);
-					expect(body).to.be.empty;
-
-					done();
+									done();
+								})
+								.catch(done);
+						})
+						.catch(done);
 				})
 				.catch(done);
 		});
