88928373cef269d5d83312316757e3a3ce2c2d67
MikeTatsuo <mike_tatsuo@hotmail.com>
2021-06-25 02:25:19 -0300

    test: refactoring user.get.test

--- test/user/user.get.test.ts
+++ test/user/user.get.test.ts
@@ -6,88 +6,59 @@ import { Endpoints, ErrorMsgs, HttpCodes } from '../../api/common';
 import { UserMock } from '../../mock';
 
 let firstUserId = 0;
-const invalidUserId = 0;
 
 const header = { Authorization: 'Bearer ' };
 const { firstUser } = UserMock;
 const { username, email, password } = firstUser;
-const { ARRAY, NUMBER, OBJECT, STRING } = EnumOfTypes;
-const { auth, user, users } = Endpoints;
-const { CREATED, FORBIDDEN, NOT_FOUND, OK, UNAUTHORIZED } = HttpCodes;
+const { NUMBER, OBJECT, STRING } = EnumOfTypes;
+const { auth, user } = Endpoints;
+const { FORBIDDEN, NOT_FOUND, OK, UNAUTHORIZED } = HttpCodes;
 const { authRequired, invalidSignature, userNotFound } = ErrorMsgs;
+const req = request(server);
 
 describe('user.get.test', () => {
-	describe(`POST ${user}`, () => {
-		it('should return 201 - Created', (done) => {
-			request(server)
-				.post(user)
-				.send(firstUser)
-				.then(({ status, body }: Response) => {
-					const { id } = body;
-
-					expect(status).to.equal(CREATED);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(id).to.be.an(NUMBER);
-					expect(body.username).to.be.an(STRING);
-					expect(body.username).to.be.equal(username);
-					expect(body.email).to.be.an(STRING);
-					expect(body.email).to.be.equal(email);
-					expect(body).not.haveOwnProperty('password');
-
-					firstUserId = id;
-
-					done();
-				})
-				.catch(done);
-		});
-	});
-
-	describe(`POST ${auth}`, () => {
-		it('should return 201 - Created', (done) => {
-			request(server)
-				.post(auth)
-				.send({ email, password })
-				.then(({ body, status }: Response) => {
-					const { accessToken, refreshToken } = body;
-
-					expect(status).to.equal(CREATED);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(accessToken).to.be.an(STRING);
-					expect(refreshToken).to.be.an(STRING);
-
-					header.Authorization += accessToken;
-					done();
-				})
-				.catch(done);
-		});
-	});
-
 	describe(`GET ${user}/:userId`, () => {
 		it('should return 200 - Ok', (done) => {
-			request(server)
-				.get(`${user}/${firstUserId}`)
-				.set(header)
-				.send()
-				.then(({ body, status }: Response) => {
-					expect(status).to.equal(OK);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(body.id).to.be.an(NUMBER);
-					expect(body.username).to.be.an(STRING);
-					expect(body.username).to.be.equal(username);
-					expect(body.email).to.be.an(STRING);
-					expect(body.email).to.be.equal(email);
-					expect(body).not.haveOwnProperty('password');
+			req
+				.post(user)
+				.send(firstUser)
+				.then(({ body }: Response) => {
+					const { id } = body;
+					firstUserId = id;
 
-					done();
+					req
+						.post(auth)
+						.send({ email, password })
+						.then(({ body }: Response) => {
+							const { accessToken } = body;
+							header.Authorization += accessToken;
+
+							req
+								.get(`${user}/${firstUserId}`)
+								.set(header)
+								.send()
+								.then(({ body, status }: Response) => {
+									expect(status).to.equal(OK);
+									expect(body).not.to.be.empty;
+									expect(body).to.be.an(OBJECT);
+									expect(body.id).to.be.an(NUMBER);
+									expect(body.username).to.be.an(STRING);
+									expect(body.username).to.be.equal(username);
+									expect(body.email).to.be.an(STRING);
+									expect(body.email).to.be.equal(email);
+									expect(body).not.haveOwnProperty('password');
+
+									done();
+								})
+								.catch(done);
+						})
+						.catch(done);
 				})
 				.catch(done);
 		});
 
 		it('should return 403 - Forbidden - invalid JWT', (done) => {
-			request(server)
+			req
 				.get(`${user}/${firstUserId}`)
 				.set({ Authorization: `${header.Authorization}123` })
 				.send()
@@ -106,7 +77,7 @@ describe('user.get.test', () => {
 		});
 
 		it('should return 401 - Unauthorized - no JWT set', (done) => {
-			request(server)
+			req
 				.get(`${user}/${firstUserId}`)
 				.send()
 				.then(({ body, status }: Response) => {
@@ -124,62 +95,28 @@ describe('user.get.test', () => {
 		});
 
 		it('should return 404 - Not Found - user not found', (done) => {
-			request(server)
-				.get(`${user}/${invalidUserId}`)
-				.set(header)
-				.send()
-				.then((res: Response) => {
-					const { status, body } = res;
-					const { error } = body;
-
-					expect(status).to.equal(NOT_FOUND);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(error).to.be.an(STRING);
-					expect(error).to.be.equal(userNotFound);
-
-					done();
-				})
-				.catch(done);
-		});
-	});
-
-	describe(`DELETE ${user}/:userId`, () => {
-		it('should return 200 - Ok', (done) => {
-			request(server)
+			req
 				.delete(`${user}/${firstUserId}`)
 				.set(header)
 				.send()
-				.then((res: Response) => {
-					const { status, body } = res;
-					const { id } = body;
+				.then(() => {
+					req
+						.get(`${user}/${firstUserId}`)
+						.set(header)
+						.send()
+						.then((res: Response) => {
+							const { status, body } = res;
+							const { error } = body;
 
-					expect(status).to.equal(OK);
-					expect(body).not.to.be.empty;
-					expect(body).to.be.an(OBJECT);
-					expect(id).to.be.an(NUMBER);
-					expect(id).to.be.equal(firstUserId);
+							expect(status).to.equal(NOT_FOUND);
+							expect(body).not.to.be.empty;
+							expect(body).to.be.an(OBJECT);
+							expect(error).to.be.an(STRING);
+							expect(error).to.be.equal(userNotFound);
 
-					done();
-				})
-				.catch(done);
-		});
-	});
-
-	describe(`GET ${users}`, () => {
-		it('should return 200 - Ok - and body to be empty', (done) => {
-			request(server)
-				.get(users)
-				.set(header)
-				.send()
-				.then((res: Response) => {
-					const { status, body } = res;
-
-					expect(status).to.equal(OK);
-					expect(body).to.be.an(ARRAY);
-					expect(body).to.be.empty;
-
-					done();
+							done();
+						})
+						.catch(done);
 				})
 				.catch(done);
 		});
